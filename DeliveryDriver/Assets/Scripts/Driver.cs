01f4e7c99593548c7f3f4d57028d5eeea2f4667d
Johan Andersson <johanstechs@gmail.com>
2022-08-07 03:47:51 +0200

    Added Delivery Driver project and some code

---
+++ DeliveryDriver/Assets/Scripts/Driver.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+  [SerializeField]
+  float turnSpeed = 200f;
+  [SerializeField]
+  float moveSpeed = 10f;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    float turnAmount = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+    float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+    transform.Rotate(0, 0, -turnAmount);
+    transform.Translate(0, moveAmount, 0);
+  }
+}
