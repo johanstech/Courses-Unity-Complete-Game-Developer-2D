a7c7332113c0e7ec4219357d1257f99453820cc2
Johan Andersson <johanstechs@gmail.com>
2022-08-08 09:58:45 +0200

    Minor cleanup

--- DeliveryDriver/Assets/Scripts/Driver.cs
+++ DeliveryDriver/Assets/Scripts/Driver.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Driver : MonoBehaviour
@@ -9,12 +7,10 @@ public class Driver : MonoBehaviour
   [SerializeField]
   float moveSpeed = 10f;
 
-  // Start is called before the first frame update
   void Start()
   {
   }
 
-  // Update is called once per frame
   void Update()
   {
     float turnAmount = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
