9fdbbbea5f49407fd5a679d09f9bf257d350c837
Johan <johanstechs@gmail.com>
2022-08-09 12:34:26 +0200

    Finished up delivery code

--- DeliveryDriver/Assets/Scripts/Delivery.cs
+++ DeliveryDriver/Assets/Scripts/Delivery.cs
@@ -3,11 +3,20 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
   [SerializeField]
+  Color32 baseColour = new Color32(1, 1, 1, 1);
+  [SerializeField]
+  Color32 hasPackageColour = new Color32(1, 1, 1, 1);
+  [SerializeField]
   float destructionDelay = 0.5f;
-
+  
+  SpriteRenderer _spriteRenderer;
+  bool _hasPackage;
   string _packageMessage = "Package picked up!";
   string _customerMessage = "Package dropped off!";
-  bool _hasPackage;
+
+  void Start() {
+    _spriteRenderer = GetComponent<SpriteRenderer>();
+  }
 
   void OnCollisionEnter2D(Collision2D other)
   {
@@ -18,15 +27,17 @@ public class Delivery : MonoBehaviour
   {
     if (other.tag == "Package" && !_hasPackage)
     {
-      _hasPackage = true;
-      Destroy(other.gameObject, destructionDelay);
       Debug.Log(_packageMessage);
+      _hasPackage = true;
+      _spriteRenderer.color = hasPackageColour;
+      Destroy(other.gameObject, destructionDelay);
     }
     
     if (other.tag == "Customer" && _hasPackage)
     {
-      _hasPackage = false;
       Debug.Log(_customerMessage);
+      _hasPackage = false;
+      _spriteRenderer.color = baseColour;
     }
   }
 }
