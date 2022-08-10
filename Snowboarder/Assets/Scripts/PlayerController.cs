5a237b9df9ef04f264bbd21a6b463730ca996731
Johan <johanstechs@gmail.com>
2022-08-10 07:10:40 +0200

    Added snow trail effects

--- Snowboarder/Assets/Scripts/PlayerController.cs
+++ Snowboarder/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,29 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     float torqueAmount = 1f;
+    [SerializeField]
+    float baseSpeed = 20f;
+    [SerializeField]
+    float boostSpeed = 30f;
+    [SerializeField]
+    float brakeSpeed = 5f;
 
     Rigidbody2D _rb2d;
+    SurfaceEffector2D _se2d;
 
     void Start()
     {
         _rb2d = GetComponent<Rigidbody2D>();
+        _se2d = FindObjectOfType<SurfaceEffector2D>();
     }
 
     void Update()
+    {
+        Rotate();
+        Boost();
+    }
+
+    void Rotate()
     {
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
@@ -21,6 +35,22 @@ public class PlayerController : MonoBehaviour
         else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             _rb2d.AddTorque(-torqueAmount);
+        }
+    }
+
+    void Boost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            _se2d.speed = boostSpeed;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            _se2d.speed = brakeSpeed;
+        }
+        else
+        {
+            _se2d.speed = baseSpeed;
         }
     }
 }
