a2174caa01bbd3af4e8b4701ebf9862991b6ff96
Johan Andersson <johanstechs@gmail.com>
2022-08-30 23:50:19 +0200

    Added boundaries to the viewport

--- LaserDefender/Assets/Scripts/Player.cs
+++ LaserDefender/Assets/Scripts/Player.cs
@@ -4,20 +4,45 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
   [SerializeField]
+  float paddingLeft = 0.5f;
+  [SerializeField]
+  float paddingRight = 0.5f;
+  [SerializeField]
+  float paddingTop = 5f;
+  [SerializeField]
+  float paddingBottom = 2f;
+  [SerializeField]
   float moveSpeed = 10f;
 
   // private variables
+  Vector2 _minBounds;
+  Vector2 _maxBounds;
   Vector2 _rawInput;
+
+  void Start()
+  {
+    InitBounds();
+  }
 
   void Update()
   {
     Move();
   }
 
+  void InitBounds()
+  {
+    Camera mainCamera = Camera.main;
+    _minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+    _maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+  }
+
   void Move()
   {
-    Vector3 delta = _rawInput * moveSpeed * Time.deltaTime;
-    transform.position += delta;
+    Vector2 delta = _rawInput * moveSpeed * Time.deltaTime;
+    Vector2 newPos = new Vector2();
+    newPos.x = Mathf.Clamp(transform.position.x + delta.x, _minBounds.x + paddingLeft, _maxBounds.x - paddingRight);
+    newPos.y = Mathf.Clamp(transform.position.y + delta.y, _minBounds.y + paddingBottom, _maxBounds.y - paddingTop);
+    transform.position = newPos;
   }
 
   void OnMove(InputValue value)
